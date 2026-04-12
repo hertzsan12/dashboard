635a16379deffd6ec79f2d8dc9853ce8174b1e23
hertzsan12 <hertzsan12.ds@gmail.com>
2026-04-12 11:29:19 +0300

    app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def log_transaction(action, item, qty, person, mdr, equipment, uom):
 # UI
 # =========================
 st.set_page_config(layout="wide")
-st.sidebar.title("Inventory System")
+st.sidebar.title("David Hertz Inventory System")
 
 menu = ["Inventory", "Equipment", "Withdraw/Deliver", "Transactions"]
 choice = st.sidebar.radio("Go to", menu)
