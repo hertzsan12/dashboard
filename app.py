128e62f1bf64aaabf9a28618e6a5d3b0390d9bfc
hertzsan12 <hertzsan12.ds@gmail.com>
2026-04-15 10:01:08 +0300

    app.py

--- app.py
+++ app.py
@@ -208,16 +208,16 @@ if choice == "Inventory":
         qty = inventory[item]
         uom = uoms.get(item, "pcs")
 
-                status = "🟢 OK"
+        status = "🟢 OK"
 
-        		if qty == 0:
-            		status = "🔴 No Stock"
-       			 elif qty <= LOW_STOCK_THRESHOLD:
-           			 status = "🟡 Low Stock"
+        if qty == 0:
+            status = "🔴 No Stock"
+       	elif qty <= LOW_STOCK_THRESHOLD:
+           	status = "🟡 Low Stock"
 
-        			data.append({"Item": item, "Quantity": qty, "UOM": uom, "Status": status})
+        	data.append({"Item": item, "Quantity": qty, "UOM": uom, "Status": status})
 
-    			st.dataframe(pd.DataFrame(data))
+    	st.dataframe(pd.DataFrame(data))
 
 # =========================
 # EQUIPMENT
